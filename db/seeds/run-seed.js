c04621dc718cfef577e34d552ff5d439f01233ef
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-11-01 10:44:31 +0000

    completes db seeding

--- db/seeds/run-seed.js
+++
@@ -1,9 +0,0 @@
-const data = require("../data/development-data/index.js");
-const seed = require("./seed.js");
-const db = require("../connection.js");
-
-const runSeed = () => {
-  return seed(data).then(() => db.end());
-};
-
-runSeed();
