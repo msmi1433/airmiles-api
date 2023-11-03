d6209099400f8e21d8a1256643e986af0d10e6b3
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-11-03 11:03:20 +0000

    prepares for hosting

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,14 @@ require("dotenv").config({
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set");
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
