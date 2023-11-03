f11ae49b5a27f24ed41bd2aea8bc9885b6a43507
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-11-03 10:52:54 +0000

    adds user tables to seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,20 +1,46 @@
 const db = require("../connection");
 
-const seed = async () => {
-  await db.query("DROP TABLE IF EXISTS british_airways;");
-  await db.query(`
-        CREATE TABLE british_airways (
+const seed = () => {
+  return db
+    .query("DROP TABLE IF EXISTS british_airways CASCADE;")
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS users CASCADE;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS users_ba_destinations;");
+    })
+    .then(() => {
+      return db.query(`
+    CREATE TABLE british_airways (
+        id SERIAL PRIMARY KEY,
+        city VARCHAR,
+        country VARCHAR,
+        economy_op INT,
+        economy_p INT,
+        p_economy_op INT,
+        p_economy_p INT,
+        business_op INT,
+        business_p INT
+    );`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY,
-            city VARCHAR,
-            country VARCHAR,
-            economy_op INT,
-            economy_p INT,
-            p_economy_op INT,
-            p_economy_p INT,
-            business_op INT,
-            business_p INT
+            username VARCHAR,
+            password_hash VARCHAR
         );`);
-  return await db.query(`
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users_ba_destinations (
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id),
+            destination_id INT REFERENCES british_airways(id)
+        );`);
+    })
+    .then(() => {
+      return db.query(`
         COPY british_airways(
             city,
             country,
@@ -25,6 +51,7 @@ const seed = async () => {
             business_op,
             business_p) 
         FROM '${__dirname}/../data/bachart.csv' DELIMITER ',' CSV HEADER;`);
+    });
 };
 
 module.exports = seed;
